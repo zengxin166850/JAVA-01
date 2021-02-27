6570daf794c2c4fb620120bb4451685a83e99d1d
zengxin <1445216422@qq.com>
2021-02-27 15:55:41 +0800

    7周作业准备

---
+++ Week_07/homework07/src/test/java/com/zengxin/homework07/Homework07ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zengxin.homework07;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework07ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
