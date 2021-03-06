4d4f55933c3bf0209d25ad38bb30c1932c01ef4b
zengxin <1445216422@qq.com>
2021-03-06 15:46:10 +0800

    14-3 读写分离 - 数据库框架版本2.0

---
+++ Week_07/homework0703/src/main/java/com/zengxin/homework0703/Homework0703Application.java
@@ -0,0 +1,13 @@
+package com.zengxin.homework0703;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework0703Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework0703Application.class, args);
+	}
+
+}
---
+++ Week_07/homework0703/src/test/java/com/zengxin/homework0703/Homework0703ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zengxin.homework0703;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework0703ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
