db18c02a25e4da3c0e0e18f2fedeb51b565d7373
zengxin <1445216422@qq.com>
2021-04-26 23:15:59 +0800

    补交22课 实现计数器，模拟扣减库存

--- Week_11/homework/src/main/java/com/zengxin/homework/redis/RedisCounter.java
+++ Week_11/homework/src/main/java/com/zengxin/homework/redis/RedisCounter.java
@@ -3,39 +3,95 @@ package com.zengxin.homework.redis;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * redis 计数器
+ */
 public class RedisCounter {
 
+    /**
+     * redis_key
+     */
     private String key;
+    /**
+     * 初始化 count
+     */
+    private String count;
+    /**
+     * jedis连接池
+     */
     private JedisPool jedisPool;
 
-    public RedisCounter(JedisPool jedisPool, String key) {
+    /**
+     * 构造并初始化
+     *
+     * @param jedisPool jedis连接池
+     * @param key       key
+     * @param count     count
+     */
+    public RedisCounter(JedisPool jedisPool, String key, String count) {
         this.jedisPool = jedisPool;
+        this.count = count;
         this.key = key;
+        init();
     }
 
-    public void initCount(int count) {
+    public void init() {
+        Objects.requireNonNull(jedisPool);
         try (Jedis jedis = jedisPool.getResource()) {
             jedis.set(key, count + "");
         }
     }
 
-    public void increase() {
-        String luaScript = ""
-                + "\nlocal v = redis.call('GET', KEYS[1]);"
-                + "\nlocal r= 0;"
-                + "\nif v == ARGV[1] then"
-                + "\nr =redis.call('DEL',KEYS[1]);"
-                + "\nend"
-                + "\nreturn r";
+    /**
+     * 原子自增
+     *
+     * @return count
+     */
+    public Long increase() {
+        try (final Jedis jedis = jedisPool.getResource()) {
+            return jedis.incr(key);
+        }
     }
 
-    public void decrease() {
-        String luaScript = ""
-                + "\nlocal v = redis.call('GET', KEYS[1]);"
-                + "\nlocal r= 0;"
-                + "\nif v == ARGV[1] then"
-                + "\nr =redis.call('DEL',KEYS[1]);"
-                + "\nend"
-                + "\nreturn r";
+    /**
+     * 递减
+     *
+     * @return 库存
+     */
+    public Long decrease() {
+        try (final Jedis jedis = jedisPool.getResource()) {
+            return jedis.decr(key);
+        }
+
     }
+
+    /**
+     * 库存扣减
+     *
+     * @return 库存
+     */
+    public Long decreaseInventory() {
+        try (final Jedis jedis = jedisPool.getResource()) {
+            String luaScript = ""
+                    + "\nlocal inventory = redis.call('GET', KEYS[1]);"
+                    + "\nlocal r = 0;"
+                    + "\nif tonumber(inventory) == 0 then"
+                    + "\nerror \"inventory not enough\""
+                    + "\nelse"
+                    + "\nr =redis.call('DECR',KEYS[1]);"
+                    + "\nend"
+                    + "\nreturn r";
+            List<String> args = new ArrayList<>();
+            args.add(key);
+            final Long r = (Long) jedis.eval(luaScript, args, new ArrayList<>());
+            System.out.println("curr inventory:" + r + " " + Thread.currentThread() + ": 秒杀成功...");
+            return r;
+        }
+    }
+
+
 }
