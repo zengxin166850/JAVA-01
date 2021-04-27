2a4eda191f360ea7d406eea44e54000b6d3de09c
zengxin <1445216422@qq.com>
2021-04-28 00:18:02 +0800

    26课 JMS消息补交

--- Week_12/README.md
+++ Week_12/README.md
@@ -1,1 +1,18 @@
-学习笔记
+# 第 24 课作业实践
+
+- [x] 1、（必做）搭建ActiveMQ服务，基于JMS，写代码分别实现对于queue和topic的消息
+  生产和消费，代码提交到github。
+- [ ] 2、（选做）基于数据库的订单表，模拟消息队列处理订单：
+  1）一个程序往表里写新订单，标记状态为未处理(status=0);
+  2）另一个程序每隔100ms定时从表里读取所有status=0的订单，打印一下订单数据，
+  然后改成完成status=1；
+- [ ] 3）（挑战☆）考虑失败重试策略，考虑多个消费程序如何协作。
+  3、（选做）将上述订单处理场景，改成使用ActiveMQ发送消息处理模式。
+  4、（选做）使用java代码，创建一个ActiveMQ Broker Server，并测试它。
+- [ ] 5、（挑战☆☆）搭建ActiveMQ的network集群和master-slave主从结构。
+- [ ] 6、（挑战☆☆☆）基于ActiveMQ的MQTT实现简单的聊天功能或者Android消息推送。
+- [ ] 7、（挑战☆）创建一个RabbitMQ，用Java代码实现简单的AMQP协议操作。
+- [ ] 8、（挑战☆☆）搭建RabbitMQ集群，重新实现前面的订单处理。
+- [ ] 9、（挑战☆☆☆）使用Apache Camel打通上述ActiveMQ集群和RabbitMQ集群，实
+  现所有写入到ActiveMQ上的一个队列q24的消息，自动转发到RabbitMQ。
+- [ ] 10、（挑战☆☆☆）压测ActiveMQ和RabbitMQ的性能。
---
+++ Week_12/homework/src/test/java/com/zengxin/homework/HomeworkApplicationTests.java
@@ -0,0 +1,70 @@
+package com.zengxin.homework;
+
+import org.apache.activemq.ActiveMQConnection;
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.command.ActiveMQQueue;
+import org.apache.activemq.command.ActiveMQTopic;
+import org.junit.jupiter.api.Test;
+
+import javax.jms.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+//@SpringBootTest
+class HomeworkApplicationTests {
+
+
+	@Test
+	void queueTest() {
+		ActiveMQQueue mqQueue = new ActiveMQQueue("test.queue");
+		testDestination(mqQueue);
+	}
+
+
+	@Test
+	void topicTest() {
+		ActiveMQTopic mqTopic = new ActiveMQTopic("test.topic");
+		testDestination(mqTopic);
+	}
+
+	public static void testDestination(Destination destination) {
+		try {
+			// 创建连接和会话
+			ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+			ActiveMQConnection conn = (ActiveMQConnection) factory.createConnection();
+			conn.start();
+			Session session = conn.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+
+			// 创建消费者
+			MessageConsumer consumer = session.createConsumer( destination ,"");
+			final AtomicInteger count = new AtomicInteger(0);
+			MessageListener listener = new MessageListener() {
+				public void onMessage(Message message) {
+					try {
+						// 打印所有的消息内容
+						System.out.println(count.incrementAndGet() + " => receive from " + destination.toString() + ": " + message);
+						 message.acknowledge(); // 前面所有未被确认的消息全部都确认。
+
+					} catch (Exception e) {
+						e.printStackTrace(); // 不要吞任何这里的异常，
+					}
+				}
+			};
+			// 绑定消息监听器
+			consumer.setMessageListener(listener);
+//			consumer.receive();
+			// 创建生产者，生产100个消息
+			MessageProducer producer = session.createProducer(destination);
+			int index = 0;
+			while (index++ < 100) {
+				TextMessage message = session.createTextMessage(index + " message.");
+				producer.send(message);
+			}
+
+			Thread.sleep(2000);
+			session.close();
+			conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
